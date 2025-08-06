b04ac0f33cf4e0602b841b795f1969f7f1588de6
Chinedubc10 <s2500151@edu.bc.fi>
2025-08-06 06:14:11 +0100

    Reset everything

--- src/app/hosts/dashboard/layout.tsx
+++ src/app/hosts/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import Sidebar from "@/components/hostse/sidebar";
+import Sidebar from "@/components/navigation/hosts/sidebar";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
