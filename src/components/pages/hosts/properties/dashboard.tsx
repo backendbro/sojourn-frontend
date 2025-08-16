0025e72528533c401c0f2f5470a4b541bb5a88d6
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-08-16 06:11:35 +0100

    Adding a metrics component

--- src/components/pages/hosts/properties/dashboard.tsx
+++ src/components/pages/hosts/properties/dashboard.tsx
@@ -8,6 +8,7 @@ import dynamic from "next/dynamic";
 import Link from "next/link";
 import { useState, MouseEvent, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import BookingMetrics from "./BookingMetrics";
 
 const Inspections = dynamic(() => import("./inspection-table"), { ssr: false });
 const Properties = dynamic(() => import("./properties-table"), { ssr: false });
@@ -43,36 +44,46 @@ export default () => {
   );
 
   return (
-    <div className="w-full py-[50px] px-8">
-      <div className="w-full flex flex-col space-y-7 justify-between md:flex-row md:items-center md:space-y-0">
-        <div className="w-full min-h-[40px] flex items-center md:w-2/5">
-          <button
-            onClick={onTabChange("inspections")}
-            className={`w-1/2 border-b-2 border-b-red-200 pb-4 ${
-              isCurrentInspections && "border-b-red-600 text-primary font-bold"
-            }`}
-          >
-            Inspections
-          </button>
-          <button
-            onClick={onTabChange("properties")}
-            className={`w-1/2 border-b-2 border-b-red-200 pb-4 ${
-              isCurrentProperties && "border-b-red-600 text-primary font-bold"
-            }`}
-          >
-            Properties
-          </button>
+    // <div className="w-full py-[50px] px-8">
+    //   <div className="w-full flex flex-col space-y-7 justify-between md:flex-row md:items-center md:space-y-0">
+    //     <div className="w-full min-h-[40px] flex items-center md:w-2/5">
+    //       <button
+    //         onClick={onTabChange("inspections")}
+    //         className={`w-1/2 border-b-2 border-b-red-200 pb-4 ${
+    //           isCurrentInspections && "border-b-red-600 text-primary font-bold"
+    //         }`}
+    //       >
+    //         Inspections
+    //       </button>
+    //       <button
+    //         onClick={onTabChange("properties")}
+    //         className={`w-1/2 border-b-2 border-b-red-200 pb-4 ${
+    //           isCurrentProperties && "border-b-red-600 text-primary font-bold"
+    //         }`}
+    //       >
+    //         Properties
+    //       </button>
+    //     </div>
+    //     <Link
+    //       href="/hosts/dashboard/properties/create"
+    //       className="flex items-center justify-center space-x-1 bg-primary text-white rounded-full px-5 py-3 ease duration-300 hover:bg-red-700"
+    //     >
+    //       <span>Create a Listing</span>
+    //       <Plus color="white" size={18} />
+    //     </Link>
+    //   </div>
+    //   {!isInspectionFormOpen &&
+    //     (tabState === "inspections" ? <Inspections /> : <Properties />)}
+    // </div>
+
+    <div>
+      <main className="py-8">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 space-y-12">
+          <BookingMetrics />
+          {/* <PropertiesDashboard />
+            <PropertyPreview /> */}
         </div>
-        <Link
-          href="/hosts/dashboard/properties/create"
-          className="flex items-center justify-center space-x-1 bg-primary text-white rounded-full px-5 py-3 ease duration-300 hover:bg-red-700"
-        >
-          <span>Create a Listing</span>
-          <Plus color="white" size={18} />
-        </Link>
-      </div>
-      {!isInspectionFormOpen &&
-        (tabState === "inspections" ? <Inspections /> : <Properties />)}
+      </main>
     </div>
   );
 };
