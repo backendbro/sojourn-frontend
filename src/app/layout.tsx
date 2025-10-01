0577901bf7def3710891d097bc4ab2722cbb4f28
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-10-01 10:39:12 +0100

    Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </head>
       <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-E34PC8GDB6"
