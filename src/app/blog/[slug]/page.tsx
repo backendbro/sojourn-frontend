55d890eeb29af50addfbbcae2c3247547f1a12b6
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-12-23 00:21:51 +0100

    Added new contents

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,13 +1,9 @@
-// app/blog/[slug]/page.tsx
 import { notFound } from "next/navigation";
 import Link from "next/link";
 
 import { getAllPosts, getPostContent } from "../../../../lib/posts";
 
-import {
-  createGradientStyle,
-  getCategoryImage,
-} from "../../../../lib/blog-utils";
+import { createGradientStyle, getCategoryImage } from "../../../../lib/blog-ui";
 
 import { categoryIcons } from "../../../components/blog/CategoryIcons";
 export async function generateStaticParams() {
