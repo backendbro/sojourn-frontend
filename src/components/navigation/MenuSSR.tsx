d5585e93dd52843487d8e6dddc4c1364b0a0b7a9
backendbro <ukagha.nzubechukwu@yahoo.com>
2026-03-02 10:59:31 -0800

    I am done with this

--- src/components/navigation/MenuSSR.tsx
+++ src/components/navigation/MenuSSR.tsx
@@ -7,7 +7,9 @@ const MenuSSR = () => {
      
     //<div className="w-full px-5 h-[67px] max-w-[1400px] 2xl:mx-auto bg-paper flex items-center justify-between sm:px-[80px]">
     <div className="w-full px-3 sm:px-5 md:px-10 lg:px-[80px] min-h-[64px] sm:min-h-[72px] md:min-h-[80px] max-w-[1400px] 2xl:mx-auto bg-primary flex items-center justify-between gap-3 sm:gap-4"> 
-    <MobileSearch />
+    <div className="shrink-0">
+        <MobileSearch />
+      </div>
       <div className="flex items-center">
          <div className="flex-1 flex items-center justify-center min-w-0">
         <MenuSearch />
