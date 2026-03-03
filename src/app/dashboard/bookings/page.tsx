92bafebc1f8e2c885ed4969e748fd29bcf176031
backendbro <ukagha.nzubechukwu@yahoo.com>
2026-03-03 03:05:09 +0100

    Pushed the booking page a bit down

--- src/app/dashboard/bookings/page.tsx
+++ src/app/dashboard/bookings/page.tsx
@@ -1,3 +1,29 @@
+// "use client";
+
+// import dynamic from "next/dynamic";
+// import { Suspense } from "react";
+
+// const BookingsTable = dynamic(
+//   () => import("@/components/pages/bookings/bookings-table"),
+//   { ssr: false }
+// );
+
+// export default () => {
+//   return (
+//     <div className="w-full py-[50px] px-8 ">
+//       <div className="w-full flex items-center justify-center">
+//         <div className="w-full relative rounded-md">
+//           <h3 className="text-[2rem] sm:text-[2.5rem] text-black">Bookings</h3>
+//         </div>
+//       </div>
+//       <Suspense fallback={<></>}>
+//         <BookingsTable />
+//       </Suspense> 
+//     </div>
+//   );
+// };
+
+
 "use client";
 
 import dynamic from "next/dynamic";
@@ -10,10 +36,10 @@ const BookingsTable = dynamic(
 
 export default () => {
   return (
-    <div className="w-full py-[50px] px-8 ">
+    <div className="w-full py-6 sm:py-[50px] px-3 sm:px-6 md:px-8">
       <div className="w-full flex items-center justify-center">
         <div className="w-full relative rounded-md">
-          <h3 className="text-[2rem] sm:text-[2.5rem] text-black">Bookings</h3>
+          <h3 className="text-xl sm:text-[2rem] md:text-[2.5rem] text-black">Bookings</h3>
         </div>
       </div>
       <Suspense fallback={<></>}>
